b5c0d35b7594c29ae9d5202cf3c2f1d0040b531b
Shekhar Kumar Gupta <shekhar114@gmail.com>
2014-04-10 22:09:18 +0530

    Revert "Listner added and works properly"
    This reverts commit 7f86002f5325162605125c57f7f5cb5627958af2.

--- app/src/main/java/com/pricetag/app/MainActivity.java
+++ app/src/main/java/com/pricetag/app/MainActivity.java
@@ -3,17 +3,23 @@ package com.pricetag.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 import com.etsy.android.grid.StaggeredGridView;
 
 
-public class MainActivity extends ActionBarActivity implements AbsListView.OnItemClickListener{
+public class MainActivity extends ActionBarActivity implements AbsListView.OnItemClickListener, AbsListView.OnScrollListener {
 
     StaggeredGridView myGridView;
     GridAdapterMainActivity myAdapter;
+    LayoutInflater getLayoutFooter;
+    View footer;
+    TextView txtFooterTitle;
+    String postUrl;
     String[] shownToDrawerUrl, shownToDrawer;
 
     @Override
@@ -23,10 +29,15 @@ public class MainActivity extends ActionBarActivity implements AbsListView.OnIte
         shownToDrawer = getResources().getStringArray(R.array.drawer_menu);
         shownToDrawerUrl = getResources().getStringArray(R.array.drawer_menu_url);
         myGridView = (StaggeredGridView) findViewById(R.id.grid_view);
+        getLayoutFooter = getLayoutInflater();
+        footer = getLayoutFooter.inflate(R.layout.list_item_header_footer, null);
+        txtFooterTitle =  (TextView) footer.findViewById(R.id.txt_title);
+        txtFooterTitle.setText(" ");
+        myGridView.addFooterView(footer);
         myAdapter = new GridAdapterMainActivity(this, R.id.txt_line);
         myGridView.setAdapter(myAdapter);
+        myGridView.setOnScrollListener(this);
         myGridView.setOnItemClickListener(this);
-        setTitle("Browse Categories");
         String image = null;
         for(int i=0; i<6; i++){
             myAdapter.add(new ProductData(shownToDrawer[i], image, shownToDrawerUrl[i]));
@@ -44,5 +55,15 @@ public class MainActivity extends ActionBarActivity implements AbsListView.OnIte
             setIntentProdId.putExtra("selectPosition", position);
             startActivity(setIntentProdId);
         }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
     }
 }
